885729a1ba5c4fd093b872aca3ec5829180615fe
barreto <gbarreto@keener.io>
2022-07-13 22:23:11 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    css:[
+        'spectre.css/dist/spectre-exp.css',
+        'spectre.css/dist/spectre-icons.css',
+        'spectre.css/dist/spectre.css'
+    ]
+})
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>H1 Heading</h1>
+    <h1>H1 Heading <small class="label">40px</small></h1>
+    <span class="h1">H1 Heading</span>
+  </div>
+</template>
